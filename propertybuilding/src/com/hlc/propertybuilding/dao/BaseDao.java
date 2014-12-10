b219efb19bbb9ced4fa161ff3b01055b2c35d3b8
lin <lin.maple.leaf@gmail.com>
2014-12-10 19:01:30 +0800

    分包

---
+++ propertybuilding/src/com/hlc/propertybuilding/dao/BaseDao.java
@@ -0,0 +1,27 @@
+package com.hlc.propertybuilding.dao;
+
+
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BaseDao<T> {
+	private Class<T> entityClass;
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	public BaseDao(){
+		Type genType =getClass().getGenericSuperclass();
+		Type[] params = ((ParameterizedType)genType).getActualTypeArguments();
+		entityClass = (Class)params[0];
+	}
+	
+//	public T load(Serializable id){
+//		return sqlSessionTemplate.sele
+//	}
+	
+	
+}
