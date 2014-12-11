d914a26cec5efd5faf2ea56ec5866c5315297486
lin <lin.maple.leaf@gmail.com>
2014-12-11 18:50:11 +0800

    基类代码

---
+++ propertybuilding/src/com/hlc/propertybuilding/domain/TFloor.java
@@ -0,0 +1,89 @@
+package com.hlc.propertybuilding.domain;
+
+import java.io.Serializable;
+
+/**
+ * TFloor entity. @author MyEclipse Persistence Tools
+ */
+public class TFloor  implements Serializable {
+
+	// Fields
+
+	private Integer flooId;
+	private Integer flooBuilding;
+	private Integer flooOrder;
+	private String flooNo;
+	private Integer flooType;
+	private float flooHeight;
+
+	// Constructors
+	/** default constructor */
+	public TFloor() {
+	}
+
+	/** minimal constructor */
+	public TFloor(Integer flooId, Integer flooBuilding, String flooNo,
+			Integer flooType) {
+		super();
+	}
+
+	/** full constructor */
+	public TFloor(Integer flooId, Integer flooBuilding, Integer flooOrder,
+			String flooNo, Integer flooType, float flooHeight) {
+		this.flooId = flooId;
+		this.flooBuilding = flooBuilding;
+		this.flooOrder = flooOrder;
+		this.flooNo = flooNo;
+		this.flooType = flooType;
+		this.flooHeight = flooHeight;
+	}
+
+	public Integer getFlooId() {
+		return flooId;
+	}
+
+	public void setFlooId(Integer flooId) {
+		this.flooId = flooId;
+	}
+
+	public Integer getFlooBuilding() {
+		return flooBuilding;
+	}
+
+	public void setFlooBuilding(Integer flooBuilding) {
+		this.flooBuilding = flooBuilding;
+	}
+
+	public Integer getFlooOrder() {
+		return flooOrder;
+	}
+
+	public void setFlooOrder(Integer flooOrder) {
+		this.flooOrder = flooOrder;
+	}
+
+	public String getFlooNo() {
+		return flooNo;
+	}
+
+	public void setFlooNo(String flooNo) {
+		this.flooNo = flooNo;
+	}
+
+	public Integer getFlooType() {
+		return flooType;
+	}
+
+	public void setFlooType(Integer flooType) {
+		this.flooType = flooType;
+	}
+
+	public float getFlooHeight() {
+		return flooHeight;
+	}
+
+	public void setFlooHeight(float flooHeight) {
+		this.flooHeight = flooHeight;
+	}
+
+}
